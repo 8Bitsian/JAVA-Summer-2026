c2930c9f6637655c3cb797b765e5c7681e4b5d88
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-05-26 16:58:11 -0400

    Update J201_1.java

--- lesson01/J201_1.java
+++ lesson01/J201_1.java
@@ -1,6 +1,18 @@
 /*
   Imani Hollie 05/26/26
-  5-Minute Interveiw - Calculate a Sum
-  Directions: Calculate the sum of the following series
-  of numbers: 10 + 12 + 14 + 16 + 18 + ___ + 100
+  5-Minute Challenge - Calculate the sum of the following series of numbers:
+  10 + 12 + 14 + 16 + 18 + ___ + 100
 */
+
+public class J201_1 {
+  public static void main(String[] args) {
+    // Process
+    int sum = 0;
+    for (int i = 10; i <= 100; i += 2)
+      sum += i;
+    
+    // Output
+    System.out.println(sum);
+    return 0;
+  }
+}
